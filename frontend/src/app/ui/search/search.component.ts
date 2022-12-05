90be51e2229f18acf635d8dc042c78a0b31bdb04
Rodrigo9602 <119463817+Rodrigo9602@users.noreply.github.com>
2022-12-04 22:31:41 -0500

    Update search.component.ts

--- frontend/src/app/ui/search/search.component.ts
+++ frontend/src/app/ui/search/search.component.ts
@@ -11,31 +11,35 @@ import Swal from 'sweetalert2';
 })
 export class SearchComponent implements OnInit {
   public serialNumber;
-  public gateway:Gateway;
+  public gateway: Gateway;
   constructor(
-    private _gatewayService:GatewayService,
+    private _gatewayService: GatewayService,
   ) {
-    this.serialNumber='';
-    this.gateway=  new Gateway('','','',[]);
-   }
+    this.serialNumber = '';
+    this.gateway = new Gateway('', '', '', []);
+  }
 
   ngOnInit(): void {
 
   }
-  onSubmit(){
-    this._gatewayService.getGatewayBySerial(this.serialNumber).subscribe(
-      res=>{ 
-        if(res.status=='success'){
-          Swal.fire('Founded!', '', 'success');
-         }        
-        this.gateway=res.gateway[0];        
-      },
-      err=>{
-        console.log(err);
-        if(err.status==404){
-          Swal.fire('Gateway not found!', 'Missing or wrong data', 'error');
-         }
-      }
-    )
+  onSubmit() {
+    if (this.serialNumber === '') {
+      Swal.fire('You need to enter some data first', 'Missing gateways serial', 'info');
+    } else {
+      this._gatewayService.getGatewayBySerial(this.serialNumber).subscribe(
+        res => {
+          if (res.status == 'success') {
+            Swal.fire('Founded!', '', 'success');
+          }
+          this.gateway = res.gateway[0];
+        },
+        err => {
+          console.log(err);
+          if (err.status == 404) {
+            Swal.fire('Gateway not found!', 'Missing or wrong data', 'error');
+          }
+        }
+      )
+    }
   }
 }
